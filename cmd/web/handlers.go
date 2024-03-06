6837b588a05f1ac921f70d7cd708cc7d861f3838
dataninja-python <aj@redstonema.com>
2024-03-06 11:43:01 -0500

    go playground installation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,8 +8,6 @@ import (
 	"snippetbox.ajigherighe.net/internal/models"
 	"snippetbox.ajigherighe.net/internal/validator"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 )
 
 type snippetCreateForm struct {
@@ -148,42 +146,5 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
-	// Update validation to work with new struct and form object
-	// Check that title is not blank and not more than 100 characters. If it fails either check add a message to the
-	// errors map using the field name as the key
-	if strings.TrimSpace(title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
-
-	// Check that content isn't blank
-	if strings.TrimSpace(content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	// Check that expires value matches one of the permitted values (1, 7 or 365)
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7, or 365"
-	}
-
-	// If there are errors, redisplay teh create.tmpl.html template, passing in the snippetCreateForm instance as
-	// dynamic data in the form field. Using the HTTP status code 422 Unprocessable entity when sending the response
-	// to indicate that there was a validation error.
-	if len(form.FieldErrors) > 0 {
-		data := app.newTemplateData(r)
-		data.Form = form
-		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl.html", data)
-		return
-	}
-
-	// Insert into the database using SnippetModel.Insert()
-	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-	// Redirect to the relevant snippet
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
